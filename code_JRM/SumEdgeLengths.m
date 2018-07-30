b7ccea6a5e3ce9253c2b089c61c35cad33f1a117
Joshua Mirth <jmirth@mail.colostate.edu>
2018-07-30 11:48:33 -0600

    Fixed some typos.

--- code_JRM/SumEdgeLengths.m
+++ code_JRM/SumEdgeLengths.m
@@ -1,19 +1,19 @@
 % Script for computing edge lengths from persistence intervals
 
 %% Settings. Edit these for different experiments.
-dir='UnitTriangle/';   % Directory containing ripser output (include trailing 
+dir='CantorDust3D_katrina/';   % Directory containing ripser output (include trailing 
                     % / ).
 fname='Homology_';      % File naming convention.
 ftype='.txt';           % File type
-name='Triangle with Area One';       % Name of sampled object (for figure titles)
+name='Cantor Dust in R^3';       % Name of sampled object (for figure titles)
 min=50;         % Minimum number of points sampled
 inc=50;         % Increment size in sampling
-max=10000;       % Maximum number of points sampled
+max=3400;       % Maximum number of points sampled
 dimn = 0;       % Dimension of homology to look at.
 
 %% Computations.
 % Loop through all the files and compute the sum of the edge lengths:
-points=[min:inc:max];
+points=min:inc:max;
 edges=zeros(size(points));
 for i=1:length(points)
     phintervals = ripserToArray([dir,fname,int2str(points(i)),ftype],dimn);
@@ -26,10 +26,10 @@ logedges = log10(edges);
 logpoints = log10(points);
 p = polyfit(logpoints,logedges,1);
 x_max=4.2; x_min=1;
-polyx=[x_min:.01:x_max];
+polyx=x_min:.01:x_max;
 polyy=p(1)*polyx + p(2);
 % Fancy fitting routine
-[alpha,c] = extrapolate_asymptotics(points,edges)
+[alpha,c] = extrapolate_asymptotics(points,edges);
 asyy = log10(c) + alpha*polyx;
 % Make plot of points and both fits
 figure;
@@ -40,6 +40,6 @@ plot(polyx,asyy);
 axis equal;
 grid on;
 title(['PH_',int2str(dimn),' for points from ',name]);
-xlabel('log_{10}(N');
-ylabel('log_{10}(L^i(X_n))');
+xlabel('log_{10}(N)');
+ylabel(['log_{10}(L^',int2str(dimn),'(X_n))']);
 legend(strcat('linear fit= ',num2str(p(1))),'data',strcat('asymptotic estimate=',num2str(alpha)));
--- code_JRM/ripser_script.sh
+++ code_JRM/ripser_script.sh
@@ -8,6 +8,7 @@ for (( i=$beg; i<=$end; i+=$inc))
 do
    file=UnitTriangle/points_$i.csv  # Edit this line to match filename format!
    echo "Computing homology of $file"
+   date
    outfn=UnitTriangle/Homology_$i.txt # Edit this line to match filename format!
    ripser $file --format point-cloud --dim $dimn >$outfn
 done
